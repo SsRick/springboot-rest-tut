584f29e8a314e4054e88f924f8a3cb5e62d0bdb2
Shankha Shubhra <shankha.shubhra@fioneer.com>
2024-06-23 23:38:37 +0530

    Added get, post and put methods

---
+++ rest-tutorial/src/main/java/com/rick/rest_tutorial/controller/CloudVendorAPIService.java
@@ -0,0 +1,34 @@
+package com.rick.rest_tutorial.controller;
+
+import com.rick.rest_tutorial.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+
+    CloudVendor cloudVendor;
+
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId) {
+//        return new CloudVendor("CV1", "Microsoft",
+//                "Belandur, Bengaluru","+919000090000");
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor = cloudVendor;
+        return "Cloud vendor created successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor = cloudVendor;
+        return "Cloud vendor updated successfully";
+    }
+
+}
